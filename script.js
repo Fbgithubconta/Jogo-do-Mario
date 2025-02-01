dfb644d805f94fb57d2a933eeade496c665df1db
Fbgithubconta <165731700+Fbgithubconta@users.noreply.github.com>
2025-01-31 23:12:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const game_board = document.querySelector('.game-board');
 const reset = document.querySelector('.reset');
 const scoreDisplay = document.querySelector('.score');
 let score = 0;
-let pipeSpeed = 5; // Velocidade constante dos canos
 let gameRunning = true;
 
 const jump = () => {
@@ -21,17 +20,13 @@ let lastFrameTime = 0;
 const loop = (timestamp) => {
     if (!gameRunning) return;
 
-    // Calcular o tempo entre os quadros (FPS)
-    const deltaTime = timestamp - lastFrameTime;
-    lastFrameTime = timestamp;
-
-    const mario_position = +window.getComputedStyle(mario).bottom.replace('px', ''); // Pega a posição da Mario
+    const mario_position = +window.getComputedStyle(mario).bottom.replace('px', ''); // Pega a posição do Mario
     const pipe_position = pipe.offsetLeft; // Pega a posição do cano
 
     // Colisão
     if (pipe_position <= 120 && pipe_position > 0 && mario_position < 80) {
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipe_position}px`;
+        pipe.style.animation = 'none'; // Para a animação quando colidir
+        pipe.style.left = `${pipe_position}px`; // Mantém o cano na posição de colisão
         mario.src = 'img/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
@@ -41,8 +36,7 @@ const loop = (timestamp) => {
         return;
     }
 
-    pipe.style.left = `${pipe_position - pipeSpeed}px`; // Atualiza a posição do cano com a velocidade constante
-
+    // Resetar a posição do cano
     if (pipe_position <= 0) {
         pipe.style.left = '100%'; // Reseta a posição do cano
     }
