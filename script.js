6848bfabb35022d1793af30cc5c82f8a41d6fdc7
Fbgithubconta <165731700+Fbgithubconta@users.noreply.github.com>
2025-02-03 11:11:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const pipe = document.querySelector('.pipe');
 const game_board = document.querySelector('.game-board');
 const reset = document.querySelector('.reset');
 
-const scoreDisplay = document.createElement('h2'); // Elemento de pontuação
+const scoreDisplay = document.createElement('h2');
 scoreDisplay.classList.add('score');
 game_board.appendChild(scoreDisplay);
 
@@ -15,13 +15,14 @@ const jump = () => {
     setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
-}
+};
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 80 && marioPosition <= 20) {
+    if (pipePosition > 0 && pipePosition <= 110 && marioPosition <= 70) {
+        // Colisão detectada
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
         mario.style.animation = 'none';
@@ -29,9 +30,10 @@ const loop = setInterval(() => {
         mario.src = 'img/game-over.png';
         mario.style.width = '75px';
         reset.style.display = 'flex';
-        mario.style.left = '50px'
+        mario.style.marginLeft = '50px'
         gameRunning = false;
-        clearInterval(scoreCounter); // Parar o contador de pontos
+        clearInterval(scoreCounter);
+        clearInterval(loop);
     }
 }, 10);
 
